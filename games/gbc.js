30f3a4eeeac97bd5b311cb9daff25ec7bccc757a
Fedorov Ivan <sirarthurjohnevans@live.com>
2021-04-29 19:17:40 +0300

    replace eval() to parsing function in gbc.js

--- games/gbc.js
+++ games/gbc.js
@@ -5,7 +5,18 @@ const getRandom = () => Math.trunc(Math.random() * 30);
 const operators = ['+', '-', '*'];
 const getRandInd = () => Math.floor(Math.random() * 3);
 const getExpression = () => `${getRandom()} ${operators[getRandInd()]} ${getRandom()}`;
-const correctAnswer = (arg) => String(eval(arg));
+const parseArg = (arg) => arg.split(' ');
+const correctAnswer = (arg) => {
+  const argArr = parseArg(arg);
+  switch (argArr[1]) {
+    case operators[0]:
+      return String(Number(argArr[0]) + Number(argArr[2]));
+    case operators[1]:
+      return String(Number(argArr[0]) - Number(argArr[2]));
+    default:
+      return String(Number(argArr[0]) * Number(argArr[2]));
+  }
+};
 
 console.log('What is the result of the expression?');
 gmLogic(getExpression, correctAnswer);
